6a4fdcacc05464ecfbd20a4d991a879b18969c22
ZhiqiGao-Leo <zgao93@wisc.edu>
2023-09-24 22:15:55 -0500

    update

--- HW2/DecisionTree.py
+++ HW2/DecisionTree.py
@@ -10,10 +10,12 @@ class TreeNode:
         self.right = right
         self.label = label
 
+
 def entropy(y):
     _, counts = np.unique(y, return_counts=True)
     probabilities = counts / len(y)
     return -np.sum(probabilities * np.log2(probabilities + 1e-10))
+
 
 def information_gain(y, y_left, y_right):
     H_parent = entropy(y)
@@ -23,9 +25,10 @@ def information_gain(y, y_left, y_right):
     p_right = len(y_right) / len(y)
     return H_parent - p_left * H_left - p_right * H_right
 
+
 def find_best_split(X, y):
     best_gain = 0
-    best_split = None
+    best_split = (None, None)
     for j in range(X.shape[1]):
         thresholds = np.unique(X[:, j])
         for c in thresholds:
@@ -39,12 +42,13 @@ def find_best_split(X, y):
                     best_split = (j, c)
     return best_split
 
+
 def build_tree(X, y):
-    if len(set(y)) == 1:
-        return TreeNode(label=y[0])
     if len(X) == 0:
         return TreeNode(label=1)  # Predict class 1 if no data
-    split_feature, threshold = find_best_split(X, y)
+    if len(set(y)) == 1:
+        return TreeNode(label=y[0])  # Predict class if only one class
+    split_feature, threshold = find_best_split(X, y)  # Find best split
     if split_feature is None:
         return TreeNode(label=1)  # Predict class 1 if no informative split
     mask = X[:, split_feature] >= threshold
@@ -53,6 +57,7 @@ def build_tree(X, y):
     left_subtree = build_tree(X_left, y_left)
     right_subtree = build_tree(X_right, y_right)
     return TreeNode(split_feature=split_feature, threshold=threshold, left=left_subtree, right=right_subtree)
+
 
 def predict(tree, x):
     if tree.label is not None:
@@ -75,11 +80,10 @@ def read_data(file_path):
 
 def question_2_2():
     # Generate the training set
-    data = np.array([[1.0, 2.0, 0],
-                     [1.5, 2.5, 0],
-                     [2.0, 3.0, 1],
-                     [2.5, 3.5, 1],
-                     [3.0, 4.0, 0]])
+    data = np.array([[1, 1, 0],
+                     [2, 2, 0],
+                     [1, 2, 1],
+                     [2, 1, 1]])
 
     X = data[:, :-1]
     y = data[:, -1]
@@ -89,42 +93,138 @@ def question_2_2():
 
     # Plot the training set
     plt.figure(figsize=(8, 6))
-    plt.scatter(X[y == 0][:, 0], X[y == 0][:, 1], color='red', marker='x', label='Class 0')
-    plt.scatter(X[y == 1][:, 0], X[y == 1][:, 1], color='blue', marker='o', label='Class 1')
+    plt.scatter(X[y == 0][:, 0], X[y == 0][:, 1], color='red', marker='x', label='Label 0')
+    plt.scatter(X[y == 1][:, 0], X[y == 1][:, 1], color='blue', marker='o', label='Label 1')
     plt.xlabel('x1')
     plt.ylabel('x2')
     plt.legend()
     plt.title('Training Set')
     plt.show()
-
     return tree
 
-def print_tree(node, depth=0):
+
+def question_2_3(X, y):
+    info = []
+    for j in range(X.shape[1]):
+        thresholds = np.unique(X[:, j])
+        for c in thresholds:
+            mask = X[:, j] >= c
+            y_left = y[mask]
+            y_right = y[~mask]
+            gain = information_gain(y, y_left, y_right)
+            info.append((j, c, gain))
+    return info
+
+
+def convert_tree_to_rules(node, parent_condition=""):
     if node.label is not None:
-        print(f"{'  ' * depth}Predict: Class {node.label}")
+        print(f"If {parent_condition} then Label {node.label}")
     else:
-        print(f"{'  ' * depth}Split on feature {node.split_feature} at threshold {node.threshold}")
-        print_tree(node.left, depth + 1)
-        print_tree(node.right, depth + 1)
-
-# Assuming you have already built the tree
-
-# Example usage
-if __name__ == '__main__':
-    # Example usage
+        left_condition = f"{parent_condition} and x{node.split_feature + 1} >= {node.threshold}"
+        right_condition = f"{parent_condition} and x{node.split_feature + 1} < {node.threshold}"
+        convert_tree_to_rules(node.left, left_condition)  # Removing the leading ' and '
+        convert_tree_to_rules(node.right, right_condition)  # Removing the leading ' and '
 
 
-    # # Assuming your data is in the format x1 x2 y
-    data = read_data('Homework 2 data/Druns.txt')
-
+def scatter_plot_from_dataset(data):
+    # Separate the coordinates and labels
     X = data[:, :-1]
     y = data[:, -1]
 
-    # Build the tree
-    tree = build_tree(X, y)
-    print_tree(tree)
+    # Create a scatter plot
+    plt.figure(figsize=(8, 6))
+    plt.scatter(X[y == 0][:, 0], X[y == 0][:, 1], color='red', marker='x', s=5, label='Label 0')
+    plt.scatter(X[y == 1][:, 0], X[y == 1][:, 1], color='blue', marker='o', s=5, label='Label 1')
+    plt.xlabel('x1')
+    plt.ylabel('x2')
+    plt.legend()
+    plt.title('Scatter Plot')
+    plt.show()
 
-    # # Example prediction
+
+def draw_uniform_points(tree, x1_interval, x2_interval, num_points):
+    x1_values = np.linspace(x1_interval[0], x1_interval[1], num_points)
+    x2_values = np.linspace(x2_interval[0], x2_interval[1], num_points)
+
+    X_test = np.array([[x1, x2] for x1 in x1_values for x2 in x2_values])
+    y_pred = [int(predict(tree, x)) for x in X_test]
+    plt.figure(figsize=(8, 6))
+    plt.scatter(X_test[:, 0], X_test[:, 1], c=y_pred, cmap='plasma', marker='.')
+    plt.xlim(x1_interval)
+    plt.ylim(x2_interval)
+    plt.xlabel('x1')
+    plt.ylabel('x2')
+    plt.title('Decision Boundary')
+    plt.show()
+
+
+
+if __name__ == '__main__':
+    print()
+    # question_2_2
+    # tree = question_2_2()
+    # print(tree.left, tree.right, tree.split_feature, tree.threshold, tree.label)
+
+    # question_2_3
+    # data = read_data('Homework 2 data/Druns.txt')
+    # X = data[:, :-1]
+    # y = data[:, -1]
+    # info = question_2_3(X, y)
+    # for i,j,k in info:
+    #     print(f"For candidate cut x{i+1} >= {j}, the information gain ratio is {k}")
+
+
+    # question_2_4
+    # data = read_data('Homework 2 data/D3leaves.txt')
+    # X = data[:, :-1]
+    # y = data[:, -1]
+    # tree = build_tree(X, y)
+    # print("Rules:")
+    # convert_tree_to_rules(tree)
+
+    # question_2_5
+    # data = read_data('Homework 2 data/D1.txt')
+    # X = data[:, :-1]
+    # y = data[:, -1]
+    # tree = build_tree(X, y)
+    # print("Rules:")
+    # convert_tree_to_rules(tree)
+    # data = read_data('Homework 2 data/D2.txt')
+    # X = data[:, :-1]
+    # y = data[:, -1]
+    # tree = build_tree(X, y)
+    # print("Rules:")
+    # convert_tree_to_rules(tree)
+
+    # question_2_6
+    # data = read_data('Homework 2 data/D1.txt')
+    # scatter_plot_from_dataset(data)
+    # data = read_data('Homework 2 data/D2.txt')
+    # scatter_plot_from_dataset(data)
+    data = read_data('Homework 2 data/D1.txt')
+    X = data[:, :-1]
+    y = data[:, -1]
+    tree = build_tree(X, y)
+    x1_interval = (0, 1)
+    x2_interval = (0, 1)
+    num_points = 200
+    draw_uniform_points(tree, x1_interval, x2_interval, num_points)
+    data = read_data('Homework 2 data/D2.txt')
+    X = data[:, :-1]
+    y = data[:, -1]
+    tree = build_tree(X, y)
+    x1_interval = (0, 1)
+    x2_interval = (0, 1)
+    num_points = 200
+    draw_uniform_points(tree, x1_interval, x2_interval, num_points)
+
+    # data = read_data('Homework 2 data/Druns.txt')
+    #
+    # X = data[:, :-1]
+    # y = data[:, -1]
+    #
+    # # Build the tree
+    # tree = build_tree(X, y)
     # new_data_point = np.array([1.493761, -0.753345])
     # prediction = predict(tree, new_data_point)
     # print(f"Predicted class: {prediction}")
--- HW2/test.py
+++ HW2/test.py
@@ -1,4 +1,44 @@
+import matplotlib.pyplot as plt
 import numpy as np
 
+# Define the decision boundaries
+def rule1(x1):
+    return x1 >= 10.0
 
-print(np.log2(0+1e-10))
+def rule2(x1, x2):
+    return (x1 < 10.0) and (x2 >= 3.0)
+
+def rule3(x1, x2):
+    return (x1 < 10.0) and (x2 < 3.0)
+
+# Generate a grid of points
+x1_values = np.linspace(0, 20, 400)
+x2_values = np.linspace(0, 10, 200)
+X1, X2 = np.meshgrid(x1_values, x2_values)
+
+# Apply rules to classify points
+Z = np.zeros_like(X1)
+for i in range(Z.shape[0]):
+    for j in range(Z.shape[1]):
+        if rule1(X1[i, j]):
+            Z[i, j] = 1
+        elif rule2(X1[i, j], X2[i, j]):
+            Z[i, j] = 1
+        elif rule3(X1[i, j], X2[i, j]):
+            Z[i, j] = 0
+
+# Create a filled contour plot
+plt.contourf(X1, X2, Z, alpha=0.5, levels=[-1, 0, 1], colors=('red', 'blue'))
+
+# Add labels
+plt.xlabel('x1')
+plt.ylabel('x2')
+
+# Add legend
+plt.legend(handles=[plt.Line2D([0], [0], marker='o', color='w', markerfacecolor='blue', markersize=10, label='Label 1'),
+                    plt.Line2D([0], [0], marker='o', color='w', markerfacecolor='red', markersize=10, label='Label 0')])
+
+# Show the plot
+plt.title('Decision Regions')
+plt.show()
+

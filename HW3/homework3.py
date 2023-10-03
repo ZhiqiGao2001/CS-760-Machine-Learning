8033c229128a4c54c7e01684e42d6e2f07a5d1a6
ZhiqiGao-Leo <zgao93@wisc.edu>
2023-10-02 19:25:42 -0500

    1

---
+++ HW3/homework3.py
@@ -0,0 +1,9 @@
+import numpy as np
+from collections import Counter
+import matplotlib.pyplot as plt
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+from scipy.interpolate import lagrange
+from sklearn.metrics import mean_squared_log_error
+
+
